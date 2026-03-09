c84e8113cdbae81d5be6e384fa9f86e3326af3ef
Riccardo Sartori <rikki.sarto@gmail.com>
2026-03-09 20:30:47 +0100

    feat: User constants

---
+++ src/constants/user.ts
@@ -0,0 +1,4 @@
+const DEFAULT_BANNER_URL = 'https://static-api.nkstatic.com/appdocs/4/assets/opendata/bbd8e1412f656b91db7df7aabbc1598b_TeamsBannerDeafult.png';
+const DEFAULT_AVATAR_URL = 'https://static-api.nkstatic.com/appdocs/4/assets/opendata/db32af61df5646951a18c60fe0013a31_ProfileAvatar01.png';
+
+export { DEFAULT_AVATAR_URL, DEFAULT_BANNER_URL };
